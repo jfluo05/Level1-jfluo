0be4828a12129bb0ceb271f08b0c7ad6add4909e
TJHL <league@WTS15.attlocal.net>
2017-12-05 19:54:26 -0800

    houses ip

--- src/Houses.java
+++ src/Houses.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
-
+static int hei;
 	static Robot h = new Robot("mini");
 
 public static void main(String[] args) {
@@ -17,20 +17,52 @@ public static void main(String[] args) {
 	h.turn(90);
 	h.move(400);
 	
-for(i=0;i<3;i++) {
+	for(i=0;i<10;i++) {
+	int c=num.nextInt(5);
+
+	if(c==0) {
+		color("pink");
+	}else if(c==1) {
+		color("magenta");
+	}else if(c==2) {
+		color("white");
+	}else if(c==3) {
+		color("cyan");
+	}else if(c==4) {
+		color("red");
+	}
+	
+
 		int h=num.nextInt(3);
-		house(h+100);
 		if(h==0) {
-		
+			house("small");
+		}else if(h==1) {
+			house("medium");
+		}else{
+			house("large");
 		}
 		
 	}
 
+}
+
+static void color(String colours) {
+	if(colours.equals("pink")) {
+	h.setPenColor(Color.PINK);
+	}if(colours.equals("magenta")) {
+		h.setPenColor(Color.magenta);
+		}if(colours.equals("white")) {
+			h.setPenColor(Color.WHITE);
+		}if(colours.equals("cyan")) {
+			h.setPenColor(Color.cyan);
+		}if(colours.equals("red")) {
+			h.setPenColor(Color.red);
+		}
 	
 }
 
 	static void house(String height) {
-		int hei=120;
+		hei=120;
 		if (height.equals("small")) {
 			hei = 60;
 		}
@@ -40,9 +72,9 @@ for(i=0;i<3;i++) {
 		if (height.equals("large")) {
 			hei = 250;
 		}
-
+		h.setWindowColor(Color.BLACK);
+		h.setPenWidth(100);
 		h.setAngle(0);
-		h.setPenColor(Color.cyan);
 		h.penDown();
 		h.move(hei);
 		h.turn(90);
@@ -53,5 +85,22 @@ for(i=0;i<3;i++) {
 		h.turn(270);
 		h.move(30);
 
+		
 	}
+	static void drawFlatRoof() {
+		h.turn(90);
+		h.move(40);
+		h.turn(90);
+	}
+	static void drawPointyRoof() {
+		h.turn(45);
+		h.move(28);
+		h.turn(315);
+		h.move(28);
+		h.turn(45);
+	}
+	
+
+
+
 }
