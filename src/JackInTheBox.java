d475f71d8d730458df62afa01aa6b0e4e0570edf
jfluo05 <league@iMac-2.attlocal.net>
2018-03-20 19:56:45 -0700

    JackInTheBox

--- src/JackInTheBox.java
+++ src/JackInTheBox.java
@@ -1,18 +1,26 @@
+import java.applet.AudioClip;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class JackInTheBox extends MouseAdapter{
+public class JackInTheBox implements MouseListener{
 	
 	JButton button = new JButton();
 	JFrame frame=new JFrame();
 	JPanel panel=new JPanel();
+	int count=0;
 	
 public static void main(String[] args) {
 	new JackInTheBox().start();
@@ -24,16 +32,83 @@ private void start() {
 	frame.setVisible(true);
 	frame.setSize(500, 500);
 	frame.setTitle("Jack In The Box");
-
 	frame.add(panel);
-
-	button.setText("Click ME!");
-	button.addActionListener((ActionListener) this);
 	
-	String JackPicture = "JackInTheBox.png";
+	panel.add(button);
+	
+	button.setText("Click ME!");
+	button.addMouseListener(this);
+	
+}
+
+	private void showPicture(String fileName) { 
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+	private void playSound(String soundFile) { 
+	     try {
+	          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+	          sound.play();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	System.out.println("click");	
+		count=count+1;
+
+		if(count==5) {
+		showPicture("JackInTheBox.png");
+		playSound("homer-doh.wav");
+	}
 		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
-public void actionPerformed(ActionEvent arg0) {
-	JButton buttonPressed = 
-}
-}
+
